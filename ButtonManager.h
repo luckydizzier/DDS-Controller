6e8c2b9b2d8358288cfa5bbd97363afaaae23c98
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-04-18 00:58:56 +0200

    Create ButtonManager.h

---
+++ ButtonManager.h
@@ -0,0 +1,31 @@
+// ButtonManager.h
+#ifndef BUTTON_MANAGER_H
+#define BUTTON_MANAGER_H
+#include <Arduino.h>
+
+#define BUTTON_PIN A0
+
+enum ButtonCode {
+  BUTTON_NONE,
+  BUTTON_SELECT,
+  BUTTON_UP,
+  BUTTON_DOWN
+};
+
+class ButtonManager {
+public:
+  void begin();
+  void update();
+  bool isPressed(ButtonCode code);
+  bool wasReleased(ButtonCode code);
+
+private:
+  ButtonCode current;
+  ButtonCode last;
+  unsigned long lastDebounceTime;
+  const unsigned long debounceDelay = 50;
+
+  ButtonCode readButton();
+};
+
+#endif

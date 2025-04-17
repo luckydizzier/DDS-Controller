96a579c9764f511ab3c914eb6c24eb1d63892887
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-04-18 00:59:20 +0200

    Create ButtonManager.cpp

---
+++ ButtonManager.cpp
@@ -0,0 +1,36 @@
+// ButtonManager.cpp
+#include "ButtonManager.h"
+
+void ButtonManager::begin() {
+  pinMode(BUTTON_PIN, INPUT);
+  current = BUTTON_NONE;
+  last = BUTTON_NONE;
+  lastDebounceTime = millis();
+}
+
+void ButtonManager::update() {
+  ButtonCode reading = readButton();
+  if (reading != last) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    current = reading;
+  }
+  last = reading;
+}
+
+bool ButtonManager::isPressed(ButtonCode code) {
+  return (current == code);
+}
+
+bool ButtonManager::wasReleased(ButtonCode code) {
+  return (last == code && current == BUTTON_NONE);
+}
+
+ButtonCode ButtonManager::readButton() {
+  int val = analogRead(BUTTON_PIN);
+  if (val < 50)   return BUTTON_SELECT;
+  if (val < 200)  return BUTTON_UP;
+  if (val < 400)  return BUTTON_DOWN;
+  return BUTTON_NONE;
+}

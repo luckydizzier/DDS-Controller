74a84726b28b6f32d255590413cb812573185cad
luckydizzier <134082331+luckydizzier@users.noreply.github.com>
2025-06-18 04:43:54 +0200

    MenuSystem.h

--- firmware/due/MenuSystem.h
+++ firmware/due/MenuSystem.h
@@ -1,78 +1,58 @@
-#ifndef MENU_SYSTEM_H
-#define MENU_SYSTEM_H
+// firmware/due/MenuSystem.h
+#ifndef MENUSYSTEM_H
+#define MENUSYSTEM_H
 
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include "ButtonManager.h"
 #include "EEPROMManager.h"
-#include "AD9850Driver.h"
+#include "DDSDriver.h"
 
 class MenuSystem {
 public:
-  void begin(ButtonManager* btns, EEPROMManager* eeprom, AD9850Driver* dds);
-  void update();
+    MenuSystem(LiquidCrystal& lcd, ButtonManager& buttons, EEPROMManager& eeprom, DDSDriver& dds);
+    void update();
 
 private:
-  enum MenuId {
-    MENU_ROOT = 0,
-    MENU_FREQ_SETTINGS,
-    MENU_SET_FREQ,
-    MENU_SAVE_EE,
-    MENU_LOAD_EE,
-    MENU_WAVEFORM,
-    MENU_WAVE_SINE,
-    MENU_WAVE_SQUARE,
-    MENU_WAVE_TRIANGLE,
-    MENU_OUTPUT_CTRL,
-    MENU_OUTPUT_TOGGLE,
-    MENU_SYSTEM,
-    MENU_FW_VERSION,
-    MENU_RESTORE_DEFAULTS,
-    MENU_EXIT
-  };
+    enum MenuID {
+        MAIN_MENU,
+        FREQ_SETTINGS,
+        FREQ_EDIT,
+        FREQ_SAVE,
+        FREQ_LOAD,
+        WAVEFORM_MENU,
+        WAVE_SINE,
+        WAVE_SQUARE,
+        WAVE_TRIANGLE,
+        OUTPUT_CTRL,
+        OUTPUT_ON,
+        OUTPUT_OFF,
+        SYSTEM_MENU,
+        SHOW_VERSION,
+        RESET_DEFAULTS,
+        EXIT_MENU
+    };
 
-  struct MenuItem {
-    MenuId id;
-    const char* label;
-    MenuItem* parent;
-    MenuItem* child;
-    MenuItem* next;
-    MenuItem* prev;
-    bool editable;
-    bool action;
-  };
+    struct MenuItem {
+        MenuID id;
+        const char* label;
+        MenuID parent;
+        MenuID children[3];
+        uint8_t childCount;
+        bool editable;
+    };
 
-  LiquidCrystal lcd = LiquidCrystal(LCD_PIN_RS, LCD_PIN_EN, LCD_PIN_D4, LCD_PIN_D5, LCD_PIN_D6, LCD_PIN_D7);
-  ButtonManager* buttons;
-  EEPROMManager* ee;
-  AD9850Driver* dds;
+    LiquidCrystal& lcd;
+    ButtonManager& buttons;
+    EEPROMManager& eeprom;
+    DDSDriver& dds;
 
-  MenuItem* current;
-  bool editing;
-  uint32_t freqValue;
-  uint32_t editValue;
-  uint8_t waveform;
+    MenuID current;
+    uint16_t freq;
 
-  // Menu items
-  MenuItem root;
-  MenuItem freqMenu;
-  MenuItem setFreq;
-  MenuItem saveEeprom;
-  MenuItem loadEeprom;
-  MenuItem waveMenu;
-  MenuItem sineItem;
-  MenuItem squareItem;
-  MenuItem triangleItem;
-  MenuItem outputMenu;
-  MenuItem outputToggle;
-  MenuItem systemMenu;
-  MenuItem fwVersion;
-  MenuItem restoreDefaults;
-  MenuItem exitItem;
-
-  void setupMenu();
-  void render();
-  void handleAction(MenuItem* item);
+    void display();
+    void navigate();
+    void applyAction(MenuID action);
 };
 
 #endif
